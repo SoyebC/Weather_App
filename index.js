b6f2a7b68cda5341c144368a92b5225bf3a481a1
Soyeb <soyebkhan442@gmail.com>
2025-08-08 20:47:27 +0530

    Updated JavaScript file

--- index.js
+++ index.js
@@ -1,53 +1,50 @@
- 
-document.addEventListener("DOMContentLoaded",()=>{
-    let cityinput = document.getElementById("city-input")
-    let weatherbtn = document.getElementById("getWeather-btn")
-    let weatherinfo = document.getElementById("weatherinfo")
-    let citynamedisplay = document.getElementById("city-name")
-    let tempdisplay = document.getElementById("temp")
-    let descriptiondisplay = document.getElementById("description")
-    let errormsg = document.getElementById("error-msg")
-    let Api_key = `2976925c73822f568dcc8301a4aab86c`;
+document.addEventListener("DOMContentLoaded", () => {
+  let cityinput = document.getElementById("city-input");
+  let weatherbtn = document.getElementById("getWeather-btn");
+  let weatherinfo = document.getElementById("weatherinfo");
+  let citynamedisplay = document.getElementById("city-name");
+  let tempdisplay = document.getElementById("temp");
+  let descriptiondisplay = document.getElementById("description");
+  let errormsg = document.getElementById("error-msg");
+  let Api_key = `2976925c73822f568dcc8301a4aab86c`;
 
-    weatherbtn.addEventListener("click",()=>{
-        let inputvalue = cityinput.value.trim()
-        
-        if(!inputvalue) return;
-        fetchdata(inputvalue)
-        cityinput.value = "";
+  weatherbtn.addEventListener("click", () => {
+    let inputvalue = cityinput.value.trim();
 
-        
-    })
-  async function  fetchdata(inputvalue) {
+    if (!inputvalue) return;
+    fetchdata(inputvalue);
+    cityinput.value = "";
+  });
+  async function fetchdata(inputvalue) {
     const apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${inputvalue}&appid=${Api_key}&units=metric`;
-   try {
-     let response = await fetch(apiurl)
-     console.log("RESPONSE",response);
-     
-     if(!response.ok){
-        throw new Error("Network response was not ok");
-     }
-        let data = await response.json();
-        displayweather(data);
-       
-   } catch (error) {
-    showerror();
-   }
-    
-  } 
-  function displayweather(weather){
-    //  Hide error
-        errormsg.classList.add("hidden");
-        weatherinfo.classList.remove("hidden");
+    try {
+      let response = await fetch(apiurl);
+      console.log("RESPONSE", response);
 
-        //  Fill data
-        citynamedisplay.textContent = `${weather.name}, ${weather.sys.country}`;
-        tempdisplay.textContent = `${weather.main.temp}°C`;
-        descriptiondisplay.textContent = weather.weather[0].description;
-   
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      let data = await response.json();
+      displayweather(data);
+      console.log(data);
+      return data;
+      
+    } catch (error) {
+      showerror();
+    }
   }
-  function showerror () {
-    errormsg.classList.add("hidden")
-    weatherinfo.classList.remove("hidden")
+  function displayweather(weather) {
+    //  Hide error
+    errormsg.classList.add("hidden");
+    weatherinfo.classList.remove("hidden");
+
+    //  Fill data
+    citynamedisplay.textContent = `${weather.name}, ${weather.sys.country}`;
+    tempdisplay.textContent = `${weather.main.temp}°C`;
+    descriptiondisplay.textContent = weather.weather[0].description;
   }
-})
+  function showerror() {
+    errormsg.classList.add("hidden");
+    weatherinfo.classList.remove("hidden");
+  }
+});
